294f518d0abc84d78175c6583be85d363ae91a12
Pavel Gorshenin <mr.gorshenya.job@gmail.com>
2022-01-03 15:36:12 +0300

    backbone is formed

---
+++ country-comparison/data_zoo.py
@@ -0,0 +1,49 @@
+socio = {
+    'density': None,
+    'median_age': None,
+    'genocide_rate': None,
+    'happiness_index': None,
+    'drug_overdose': None
+}
+
+safety = {
+    'homicide_rate': None,
+    'inprisoned': None,
+    'car_death': None,
+    'numbeo_safety': None
+}
+
+culture = {
+    'n_unesco': None,
+    'n_museums': None,
+    'n_caunes_movies': None
+}
+
+education = {
+    'university_ranking': None,
+    'nobile_laureats': None
+}
+
+economy = {
+    'gdp_per_cap_ppp': None,
+    'gdp_per_cap_nom': None,
+    'road_infrastructure': None,
+    'avg_inflation_rate': None,
+    'hidden_economy_ratio': None,
+    'complexity_index': None
+}
+
+environment = {
+    'weather_index': None,
+    'ecology_index': None
+}
+
+cost_of_living = {
+    'numbeo': None,
+    'extra_pays': None,
+    'is_free_education': None
+}
+
+health = {
+    'numbeo_health': None
+}
