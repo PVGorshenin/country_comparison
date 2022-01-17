a720de81b2c1713d52c702d2d92da76b78b7c085
Pavel Gorshenin <mr.gorshenya.job@gmail.com>
2022-01-17 17:49:30 +0300

    it works

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import numpy as np
+import pandas as pd
+from country_comparison.compare import default_minmax_scaling
 from country_comparison.reads import read_config
 
 config = read_config()
-print(config)
+
+df = pd.read_csv(config['match_table_filepath'])
+
+res_arr = np.zeros((df.shape[0], 1))
+
+for feature in config['features_to_use']:
+    clips = {}
+    if feature in config['feature_clips']:
+        clips = config['feature_clips'][feature]
+
+    feature_arr = df[feature].values.reshape(-1, 1)
+    res_arr += default_minmax_scaling(feature_arr, **clips)
+
+df['final_score'] = default_minmax_scaling(res_arr)
+df[['country', 'final_score']].to_csv(config['res_filepath'], index=None)
