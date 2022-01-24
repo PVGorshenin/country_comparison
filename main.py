60f6311bd4fb6f4e501335d06e8d54019fb8e6b8
pvgorshenin <mr.gorshenya.job@gmail.com>
2022-01-24 16:16:47 +0300

    numbeo renames

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from country_comparison.reads import read_config
 config = read_config()
 
 df = pd.read_csv(config['match_table_filepath'])
-
 res_arr = np.zeros((df.shape[0], 1))
 
 for feature in config['features_to_use']:
@@ -17,5 +16,6 @@ for feature in config['features_to_use']:
     feature_arr = df[feature].values.reshape(-1, 1)
     res_arr += default_minmax_scaling(feature_arr, **clips) * config['feature_weights'][feature]
 
+df['unscaled_score'] = res_arr
 df['final_score'] = default_minmax_scaling(res_arr)
-df[['country', 'final_score']].to_csv(config['res_filepath'], index=None)
+df[['country', 'unscaled_score', 'final_score']].to_csv(config['res_filepath'], index=None)
