08355a12fa91a6550d1f2999149781df970a13bb
pvgorshenin <mr.gorshenya.job@gmail.com>
2022-02-04 14:41:37 +0300

    search a body

--- country_comparison/plots/plot_rankings.py
+++ country_comparison/plots/plot_rankings.py
@@ -3,17 +3,29 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-from definitions import get_root
 from os import path
+
+
+def _highlight_basic_country(standings_df, config, ax):
+    pos = standings_df.index[standings_df['country'] == config['country_to_highlight']][0]
+    red_color = '#aa3333'
+    ax.patches[pos].set_facecolor(red_color)
+    return ax
+
+
+def highlight_buddie(standings_df, fig, ax, config, country_buddie):
+    pos = standings_df.index[standings_df['country'] == country_buddie][0]
+    magenda_color = '#ff00ff'
+    ax.patches[pos].set_facecolor(magenda_color)
+    plt.savefig(path.join(config['root'], config['res_filepath'].replace('csv', 'jpeg')))
+    st.pyplot(fig)
 
 
 def plot_rankings_hor(standings_df: pd.DataFrame, config):
     ind = np.arange(standings_df.shape[0])
 
-    root = get_root()
+    standings_df = standings_df[['country', 'final_score']]
 
-    standings_df = standings_df[['country', 'final_score']].sort_values(by='final_score',
-                                                                        ascending=True).reset_index(drop=True)
     fig, ax = plt.subplots(figsize=(12, 18))
 
     rects = ax.barh(ind, standings_df['final_score'].values, color='y')
@@ -23,10 +35,9 @@ def plot_rankings_hor(standings_df: pd.DataFrame, config):
     ax.set_xlabel("final_score")
     ax.set_title("final_score")
 
-    pos = standings_df.index[standings_df['country'] == config['country_to_highlight']][0]
-    red_color = '#aa3333'
-    ax.patches[pos].set_facecolor(red_color)
+    ax = _highlight_basic_country(standings_df, config, ax)
 
-    plt.savefig(path.join(root, config['res_filepath'].replace('csv', 'jpeg')))
-
+    plt.savefig(path.join(config['root'], config['res_filepath'].replace('csv', 'jpeg')))
     st.pyplot(fig)
+
+    return fig, ax
---
+++ country_comparison/search_a_buddie.py
@@ -0,0 +1,21 @@
+from sklearn.metrics.pairwise import euclidean_distances
+
+
+def search_a_buddie(standings_df, config):
+    start_of_values_col = 'gdp_ppp'
+    country_buddie = None
+    min_dist = 100500
+
+    inp_country = standings_df.loc[standings_df['country'] == config['country_to_highlight'],
+                                  start_of_values_col:].values
+    other_countries = standings_df.loc[standings_df['country'] != config['country_to_highlight'],
+                                      start_of_values_col:].values
+    other_countries_n =standings_df.loc[standings_df['country'] != config['country_to_highlight'],
+                                        'country'].values
+    for country_name, country_vect in zip(other_countries_n, other_countries):
+        curr_dist = euclidean_distances(inp_country.reshape(1, -1), country_vect.reshape(1, -1))
+        if curr_dist < min_dist:
+            country_buddie = country_name
+            min_dist = curr_dist
+
+    return country_buddie
