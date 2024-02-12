7c4ad8e646939e67ed5600f07a0e283b36b14070
pvgorshenin <mr.gorshenya.job@gmail.com>
2024-02-12 12:41:28 +0300

    numbeo renamed

---
+++ processing/cleaning/rename_numbeo.py
@@ -0,0 +1,24 @@
+import json
+import pandas as pd
+
+
+input_filepath = '../../data/intermediate/2023/numbeo.csv'
+output_filepath = '../../data/intermediate/2023/numbeo_renamed.csv'
+
+
+df = pd.read_csv(input_filepath)
+
+numbeo_src_cols = df.columns
+numbeo_cols = [col.lower().replace(' ', '_') for col in numbeo_src_cols]
+
+numbeo_cols = ['_'.join(['numbeo', col]) for col in numbeo_cols]
+
+rename_dct = {}
+for feature in zip(numbeo_src_cols, numbeo_cols):
+    rename_dct[feature[0]] = feature[1]
+
+df.rename(rename_dct, axis=1, inplace=True)
+
+df.to_csv(output_filepath, index=None)
+
+
