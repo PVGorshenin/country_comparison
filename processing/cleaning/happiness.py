98efc8ceae388405b7b137341617c110f7361bb0
pvgorshenin <mr.gorshenya.job@gmail.com>
2024-02-22 14:52:03 +0300

    happiness: fillup empty values

--- processing/cleaning/happiness.py
+++ processing/cleaning/happiness.py
@@ -9,10 +9,17 @@ happy_df = pd.read_excel(input_filepath)
 happy_df.rename({
     'Ladder score': 'happiness',
     'Country name': 'country',
-    'Perceptions of corruption': 'corruption'
 }, inplace=True, axis=1)
 
 cols_to_save = ['country', 'happiness']
+
+happy_df = happy_df[cols_to_save]
+#taken from different years
+happy_df.loc[happy_df.shape[0]+1] = ['Azerbaijan', 5.17]
+happy_df.loc[happy_df.shape[0]+1] = ['Belarus', 5.53]
+happy_df.loc[happy_df.shape[0]+1] = ['Kuwait', 6.1]
+happy_df.loc[happy_df.shape[0]+1] = ['Oman', 6.683]
+happy_df.loc[happy_df.shape[0]+1] = ['Qatar', 6.74]
 
 happy_df[cols_to_save].to_csv(
     output_filepath,
